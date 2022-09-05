d83bc7254f37ce22ec484005337103a81af51d64
Leandro Galeano <77251155+leagaleano@users.noreply.github.com>
2022-09-05 18:40:14 +0200

    Update CopyFiles.ps1

--- CopyFiles.ps1
+++ CopyFiles.ps1
@@ -14,3 +14,28 @@
              Write-Host "Not copying $_"
          }
      }
+
+
+Get-Content "ServerList.txt" | %{
+
+  # Define what each job does
+  $ScriptBlock = {
+    param($pipelinePassIn) 
+    Test-Path "\\$pipelinePassIn\c`$\Something"
+    Start-Sleep 60
+  }
+
+  # Execute the jobs in parallel
+  Start-Job $ScriptBlock -ArgumentList $_
+}
+
+Get-Job
+
+# Wait for it all to complete
+While (Get-Job -State "Running")
+{
+  Start-Sleep 10
+}
+
+# Getting the information back from the jobs
+Get-Job | Receive-Job
