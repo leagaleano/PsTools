d742bab0a964abadd431045edfcd95f9184152cc
Leandro Galeano <77251155+leagaleano@users.noreply.github.com>
2023-01-05 11:24:40 +0100

    Update scratchpad.ps1

--- scratchpad.ps1
+++ scratchpad.ps1
@@ -1,28 +1,58 @@
-# Set the file paths for the two files you want to compare
-$file1 = 'C:\path\to\file1.txt'
-$file2 = 'C:\path\to\file2.txt'
+# Set the organization, project, and build ID
+$organization = "myorg"
+$project = "myproject"
+$buildId = "123"
 
-# Read the contents of the two files into variables
-$contents1 = Get-Content $file1
-$contents2 = Get-Content $file2
+# Set the personal access token
+$PAT = "abcdefghijklmnopqrstuvwxyz"
 
-# Compare the contents of the two files
-$diff = Compare-Object $contents1 $contents2
-
-# Output the differences between the two files
-$diff | ForEach-Object {
-    # Determine the line number for the difference
-    if ($_.SideIndicator -eq '=>') {
-        $lineNumber = $contents2.IndexOf($_.InputObject) + 1
-        Write-Host "Line $lineNumber: [extra line in file2] $($_.InputObject)"
-    } elseif ($_.SideIndicator -eq '<=') {
-        $lineNumber = $contents1.IndexOf($_.InputObject) + 1
-        Write-Host "Line $lineNumber: [missing line in file2] $($_.InputObject)"
-    } else {
-        $lineNumber1 = $contents1.IndexOf($_.InputObject) + 1
-        $lineNumber2 = $contents2.IndexOf($_.InputObject) + 1
-        Write-Host "Line $lineNumber1: [missing line in file2] $($_.InputObject)"
-        Write-Host "Line $lineNumber2: [extra line in file2] $($_.InputObject)"
-    }
+# Set the headers for the REST API call
+$headers = @{
+    "Authorization" = "Basic $(ConvertTo-Base64 -String "$($PAT):$($PAT)")"
 }
 
+# Set the REST API endpoint
+$uri = "https://dev.azure.com/$organization/$project/_apis/build/builds/$buildId?api-version=6.0"
+
+# Make the REST API call
+$build = Invoke-RestMethod -Uri $uri -Headers $headers -Method Get
+
+# Get the build agent and agent pool
+$buildAgent = $build.queue.name
+$agentPool = $build.queue.pool.name
+
+# Print the build agent and agent pool
+Write-Output "Build agent: $buildAgent"
+Write-Output "Agent pool: $agentPool"
+
+
+#--------------------------
+
+# Set the organization, project, and agent name
+$organization = "myorg"
+$project = "myproject"
+$agentName = "myagent"
+
+# Set the personal access token
+$PAT = "abcdefghijklmnopqrstuvwxyz"
+
+# Set the headers for the REST API call
+$headers = @{
+    "Authorization" = "Basic $(ConvertTo-Base64 -String "$($PAT):$($PAT)")"
+}
+
+# Set the REST API endpoint
+$uri = "https://dev.azure.com/$organization/$project/_apis/build/builds?api-version=6.0&`$top=100&definitions=&reasonFilter=manual&resultFilter=succeeded&statusFilter=completed&queueTime=&finishTime=&requestedFor=&minTime=&maxTime=&agentName=$agentName"
+
+# Make the REST API call to get the list of builds
+$builds = Invoke-RestMethod -Uri $uri -Headers $headers -Method Get
+
+# Filter the list of builds by status and date
+$previousBuilds = $builds.value | Where-Object {$_.status -eq "completed" -and $_.finishTime -lt $build.finishTime}
+$followingBuilds = $builds.value | Where-Object {$_.status -eq "completed" -and $_.finishTime -gt $build.finishTime}
+
+# Print the previous and following builds
+Write-Output "Previous builds:"
+$previousBuilds | Select-Object -Property id,finishTime | Format-Table
+Write-Output "Following builds:"
+$followingBuilds | Select-Object -Property id,finishTime | Format-Table
